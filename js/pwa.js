2fbb4416428a7e6c5d62264d7c3ec52d90b3782e
vespiary8824 <vespiary8824@gmail.com>
2021-03-23 16:49:51 +0900

    .

--- js/pwa.js
+++ js/pwa.js
@@ -20,8 +20,8 @@ function init() {
 	if ('serviceWorker' in navigator && navigator.onLine) {
 		navigator.serviceWorker.register('/service-worker.js')
 			.then((reg) => {
-				initSubscribe(reg); //구독 등록
 				console.log('Service worker registered -->', reg);
+				initSubscribe(reg); //구독 등록
 			}, (err) => {
 				console.error('Service worker not registered -->', err);
 			});
